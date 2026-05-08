7e8763959b798d027f4420074ecd51538a405f8f
Vikash Gupta <vikashgupta67429@gmail.com>
2026-05-08 15:07:10 +0530

    Fix: trust all proxies so Laravel detects HTTPS behind Render's load balancer

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Trust all proxies (Render/Vercel terminate SSL at their load balancer)
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
